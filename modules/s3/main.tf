b70c61947c40e12b325fb15f269e6fc02e5ac5dc
sbogacz <steven.bogacz@gmail.com>
2018-04-04 10:58:08 -0600

    add suffix to replication bucket

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -54,7 +54,7 @@ resource "aws_s3_bucket" "website_replication_logging" {
 resource "aws_s3_bucket" "website_replication" {
   count    = "${var.enable_replication ? 1 : 0}"
   provider = "aws.replication"
-  bucket   = "${var.bucket_name}"
+  bucket   = "${var.bucket_name}-replication"
   acl      = "public-read"
 
   region = "${var.replication_aws_region}"
@@ -83,8 +83,6 @@ resource "aws_iam_role" "replication" {
   count = "${var.enable_replication ? 1 : 0}"
   name  = "${var.bucket_name}_replication_role"
 
-  tags = "${var.tags}"
-
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -105,8 +103,6 @@ POLICY
 resource "aws_iam_policy" "replication" {
   count = "${var.enable_replication ? 1 : 0}"
   name  = "${var.bucket_name}_replication_policy"
-
-  tags = "${var.tags}"
 
   policy = <<POLICY
 {
@@ -150,8 +146,6 @@ resource "aws_iam_policy_attachment" "replication" {
   name       = "${var.bucket_name}_replication_role_policy_attachment"
   roles      = ["${aws_iam_role.replication.name}"]
   policy_arn = "${aws_iam_policy.replication.arn}"
-
-  tags = "${var.tags}"
 }
 
 # Cross-region replicated website bucket
